aa6812f37791bada0450680e08c078ca5a411c59
Phat Nguyen <phatnguyentanit@gmail.com>
2022-09-20 18:01:06 -0500

    Init Commit

---
+++ my-portal-service/src/main/java/edu/miu/myportalservice/MyPortalServiceApplication.java
@@ -0,0 +1,13 @@
+package edu.miu.myportalservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPortalServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyPortalServiceApplication.class, args);
+    }
+
+}
---
+++ my-portal-service/src/test/java/edu/miu/myportalservice/MyPortalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.miu.myportalservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPortalServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
